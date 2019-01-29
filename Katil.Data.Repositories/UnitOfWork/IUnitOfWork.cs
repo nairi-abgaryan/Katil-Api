a7477a7b45c01de7816356d2b78dd2360590ad6c
nairi <nairi.abgaryan@gmail.com>
2019-01-29 16:46:00 +0400

    [FEAT]:UnitOfWork. UnitOfWork class and interface

---
+++ Katil.Data.Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Katil.Data.Repositories.AppUser;
+
+namespace Katil.Data.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        Task<int> Complete();
+    }
+}
---
+++ Katil.Data.Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Katil.Data.Model;
+using Katil.Data.Repositories.AppUser;
+using Microsoft.EntityFrameworkCore;
+
+namespace Katil.Data.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private IUserRepository userRepository = null;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository => userRepository ?? new UserRepository(_context);
+
+        public async Task<int> Complete()
+        {
+            using (var scope = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var res = await _context.SaveChangesAsync();
+                    scope.Commit();
+                    return res;
+                }
+                catch (DbException ex)
+                {
+                    scope.Rollback();
+                    Debug.WriteLine(ex.Message);
+                    return await Task.FromResult(ex.ErrorCode);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    throw;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
