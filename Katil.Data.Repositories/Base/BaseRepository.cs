aabe399d4d0e10aba91b46ee139a226c6a583500
nairi <nairi.abgaryan@gmail.com>
2019-01-29 16:45:34 +0400

    [FEAT]:Repository. Base repository

---
+++ Katil.Data.Repositories/Base/BaseRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Katil.Data.Model;
+using Katil.Data.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Katil.Data.Repositories.Base
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        protected BaseRepository(AppDbContext context)
+        {
+            this._context = context;
+            DbSet = context.Set<T>();
+        }
+
+        public DbSet<T> DbSet { get; set; }
+
+        protected List<string> TrackedProperties { get; set; }
+
+        protected AppDbContext Context => _context;
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<ICollection<T>> FindAllAsync(Expression<Func<T, bool>> where)
+        {
+            return await DbSet.Where(where).ToListAsync();
+        }
+
+        public async Task<T> InsertAsync(T obj)
+        {
+            var inserted = await DbSet.AddAsync(obj);
+            return inserted.Entity;
+        }
+
+        public void Update(T obj)
+        {
+            DbSet.Update(obj);
+        }
+
+        public void Attach(T obj)
+        {
+            try
+            {
+                DbSet.Attach(obj);
+                Context.Entry(obj).State = EntityState.Modified;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var obj = await DbSet.FindAsync(id);
+            var prop = obj.GetType().GetProperty("IsDeleted");
+
+            if (prop != null)
+            {
+                prop.SetValue(obj, true);
+                DbSet.Attach(obj);
+                Context.Entry(obj).State = EntityState.Modified;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<T> GetNoTrackingByIdAsync(Expression<Func<T, bool>> where)
+        {
+            return await DbSet.AsNoTracking().SingleOrDefaultAsync(where);
+        }
+    }
+}
---
+++ Katil.Data.Repositories/Base/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Katil.Data.Repositories.Base
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        DbSet<TEntity> DbSet { get; set; }
+
+        void Attach(TEntity obj);
+
+        Task<ICollection<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> where);
+
+        Task<ICollection<TEntity>> GetAllAsync();
+
+        TEntity GetById(int id);
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<TEntity> GetNoTrackingByIdAsync(Expression<Func<TEntity, bool>> where);
+
+        Task<TEntity> InsertAsync(TEntity obj);
+
+        void Update(TEntity obj);
+
+        Task<bool> Delete(int id);
+    }
+}
