4d9f4438e6ebf2f26094fcec2ed1d0d01c7853b5
nairi <nairi.abgaryan@gmail.com>
2019-01-29 16:41:59 +0400

    [FEAT]:Utilities. App settings for get configuration data

---
+++ Katil.Common.Utilities/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Katil.Common.Utilities
+{
+	public class AppSettings
+	{
+		public string JwtSignature { get; set; }
+		public string Secret { get; set; }
+		public string DbConnectionString { get; set; }
+	}
+}
