93e3456818da2a55cb4d1e25bb3df8413a2763e2
nairi <nairi.abgaryan@gmail.com>
2019-01-29 16:54:14 +0400

    [FEAT]:BaseEntity. Base Entity Model

---
+++ Katil.Data.Model/BaseEntity.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Katil.Data.Model
+{
+	public class BaseEntity
+	{
+		public DateTime? CreatedDate { get; set; }
+
+		public int? CreatedBy { get; set; }
+
+		public DateTime? ModifiedDate { get; set; }
+
+		public int? ModifiedBy { get; set; }
+	}
+}
