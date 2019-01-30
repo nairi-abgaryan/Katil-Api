2cf2ef680ce4c488d070f504623f586129ba444a
nairi <nairi.abgaryan@gmail.com>
2019-01-30 17:48:20 +0400

    [FEAT]:Registration. User Registration Request and Response type

---
+++ Katil.Business.Entities/Models/User/UserRegistrationRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Katil.Business.Entities.Models.User
+{
+    public class UserRegistrationsRequest
+    {
+        [StringLength(100)]
+        [Required]
+        [EmailAddress]
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        public string Password { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+        
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ Katil.Business.Entities/Models/User/UserRegistrationResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Katil.Business.Entities.Models.Base;
+using Katil.Common.Utilities;
+
+namespace Katil.Business.Entities.Models.User
+{
+    public abstract class UserRegistrationResponse : CommonResponse
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(100)]
+        [Required]
+        public string Email { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        public string Password { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+        
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
