8286a332416001112b26f0cc9e3b2c73c643ebd0
nairi <nairi.abgaryan@gmail.com>
2019-01-30 17:50:24 +0400

    [FEAT]:UserMapping. User Mapping

---
+++ Katil.Business.Services/Mapping/UserMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Katil.Business.Entities.Models.User;
+using Katil.Common.Utilities;
+using Katil.Data.Model;
+
+namespace Katil.Business.Services.Mapping
+{
+    public class UserMapping : Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<UserRegistrationsRequest, User>();
+            CreateMap<User, UserRegistrationResponse>()
+                .ForMember(x => x.CreatedDate, opt => opt.MapFrom(src => src.CreatedDate.ToCmDateTimeString()))
+                .ForMember(x => x.ModifiedDate, opt => opt.MapFrom(src => src.ModifiedDate.ToCmDateTimeString()));
+        }
+    }
+}
