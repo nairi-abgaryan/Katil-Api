a4d79f97dadcfd584cad749944b075c19b4c484a
nairi <nairi.abgaryan@gmail.com>
2019-01-30 17:55:22 +0400

    [FEAT]:DateTime. Add extension for DateTime

---
+++ Katil.Common.Utilities/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Katil.Common.Utilities
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime GetCmDateTime(this DateTime dt)
+        {
+            return Convert.ToDateTime(dt.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ"));
+        }
+
+        public static string ToCmDateTimeString(this DateTime? dt)
+        {
+            if (!dt.HasValue)
+            {
+                return string.Empty;
+            }
+
+            return dt.Value.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ");
+        }
+
+        public static string ToCmDateTimeString(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ");
+        }
+    }
+}
