804d0f142f90a6a7a20353e4b3fcae900ab0ec7f
nairi <nairi.abgaryan@gmail.com>
2019-01-30 17:47:07 +0400

    [FEAT]:CommonResponse. Response type for inheritance

---
+++ Katil.Business.Entities/Models/Base/CommonResponse.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace Katil.Business.Entities.Models.Base
+{
+	public class CommonResponse
+	{
+		[JsonProperty("created_date")]
+		public string CreatedDate { get; set; }
+
+		[JsonProperty("created_by")]
+		public int CreatedBy { get; set; }
+
+		[JsonProperty("modified_date")]
+		public string ModifiedDate { get; set; }
+
+		[JsonProperty("modified_by")]
+		public int ModifiedBy { get; set; }	
+	}
+}
